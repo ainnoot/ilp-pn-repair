66ccc31a1ebb7af590948f4942509c252ab33752
ainnoot <ai6chr+github@gmail.com>
2024-03-11 16:07:43 +0100

    argparse

--- learn_from_log.py
+++ learn_from_log.py
@@ -3,22 +3,37 @@ from pathlib import Path
 from ipl.aalpy_rpni_learner import RPNILearner
 from ipl.log_loader import Examples
 import sys
+from argparse import ArgumentParser
+from dataclasses import dataclass
 
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: {} [log folder] [batch size] [output folder]".format(__file__))
-        print("  * log folder: Path to folder containing pos.xes, neg.xes.")
-        print("  * batch size: Number of examples that go into the learner.")
-        print("  * output folder: Folder that stores intermediate learned models.")
-        sys.exit(1)
+@dataclass(frozen=True)
+class CLIArgs:
+    log_folder: Path
+    batch_size: int
+    output_folder: Path
+    seed: int
+    deterministic: bool
 
-    _, log_folder, bs, output_folder = sys.argv
-    bs = int(bs)
-    output_folder = Path(output_folder)
-    log_folder = Path(log_folder)
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("log_folder", type=str, help="Path to folder containing pos.xes, neg.xes.")
+    parser.add_argument("batch_size", type=int, help="Number of examples that go into the learner at a time.")
+    parser.add_argument("output_folder", type=str, help="Folder that stores intermediate learned models.")
+    parser.add_argument("-s", "--seed", type=int, default=77, help="Random seed for reproducibility.")
+    parser.add_argument("-d", "--deterministic", action="store_true", help="Make log deterministic; remove intersection between negative and positive traces.")
 
-    poslog = log_folder / 'pos.xes'
-    neglog = log_folder / 'neg.xes'
+    args = parser.parse_args()
+    args = CLIArgs(
+        Path(args.log_folder),
+        int(args.batch_size),
+        Path(args.output_folder),
+        int(args.seed),
+        bool(args.deterministic)
+    )
+
+    poslog = args.log_folder / 'pos.xes'
+    neglog = args.log_folder / 'neg.xes'
+
     if not poslog.exists():
         print("File does not exist:", poslog)
         sys.exit(1)
@@ -27,10 +42,17 @@ if __name__ == '__main__':
         print("File does not exist:", neglog)
         sys.exit(1)
 
-    examples = Examples(poslog.as_posix(), neglog.as_posix(), True, 77)
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    poslog = args.log_folder / 'pos.xes'
+    neglog = args.log_folder / 'neg.xes'
+
+    examples = Examples(poslog.as_posix(), neglog.as_posix(), args.deterministic, args.seed)
     learner = RPNILearner()
 
-    for bs_idx, E in enumerate(examples.shuffle().batch(bs)):
+    for bs_idx, E in enumerate(examples.shuffle().batch(args.batch_size)):
         print("Solving shot: {}".format(bs_idx))
         learner.learn(E)
 
@@ -39,5 +61,5 @@ if __name__ == '__main__':
             sys.exit(1)
 
         model_path = "learned_model_{}.pdf".format(bs_idx)
-        print("Saving model to: {}".format(output_folder / model_path))
-        learner.model.visualize(output_folder / model_path)
+        print("Saving model to: {}".format(args.output_folder / model_path))
+        learner.model.visualize(args.output_folder / model_path)
