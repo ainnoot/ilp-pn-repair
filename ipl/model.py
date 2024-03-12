9f044bce0ea33d67db942efdee75c3b79d23dd53
ainnoot <ai6chr+github@gmail.com>
2024-03-12 10:17:58 +0100

    lazy regexp fix on activity names

--- ipl/model.py
+++ ipl/model.py
@@ -10,7 +10,7 @@ class Trace:
 
     def __post_init__(self):
         for event in self.events:
-            ans = re.fullmatch("[a-z][a-z0-9_]*", event)
+            ans = re.fullmatch("[a-z0-9_]+", event)
             if ans is None:
                 err = "Event: {} does not match expected format: [a-z][a-z0-9_]*".format(event)
                 raise RuntimeError(err)
