ad3a0381abec76b52ae93f10e48fc57ff4a9d782
ainnoot <ai6chr+github@gmail.com>
2024-03-19 09:56:23 +0100

    example bash script w/ less arguments

--- example.sh
+++ example.sh
@@ -2,6 +2,8 @@
 
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <log folder> <output folder>"
+		echo "log folder: a folder that contains a pos.xes and neg.xes file, respectively positive and negative traces."
+		echo "output folder: learned automata will be stored here"
     exit 1
 fi
 
