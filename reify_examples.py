12bb7191a138efbc561ba4e0ea0590c386d705f7
ainnoot <ai6chr+github@gmail.com>
2024-03-23 01:00:29 +0100

    README

--- reify_examples.py
+++ reify_examples.py
@@ -1,15 +1,14 @@
-import sys
-
-from dfa_repair.examples import Examples, Example
+from pathlib import Path
+from dfa_repair.examples import Examples
 from dfa_repair.reify_as_facts import reify_as_facts
 from dfa_repair.reify_as_ilasp import ILASPExample
 from argparse import ArgumentParser
 
 def parse_args():
-    p = ArgumentParser()
-    p.add_argument("examples", type=str)
-    p.add_argument("-v", "--verbose", action="store_true")
-    p.add_argument('--ilasp', action='store_true')
+    p = ArgumentParser(prog=Path(__file__).stem)
+    p.add_argument("examples", type=str, help="Path to a JSON file with positive, negative examples.")
+    p.add_argument("-v", "--verbose", action="store_true", help="Verbose & debug output, if you're lucky.")
+    p.add_argument('--ilasp', action='store_true', help="Dump to ILASP examples instead of plain ASP facts.")
 
     return p.parse_args()
 
