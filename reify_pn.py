c550444755757890aeed10257fd994924dd6737a
ainnoot <ai6chr+github@gmail.com>
2024-04-04 18:18:48 +0200

    remapping representation in reification script

--- reify_pn.py
+++ reify_pn.py
@@ -1,3 +1,7 @@
+import sys
+
+import clingo
+
 from ilp_petri_net_repair import PetriNetReification
 from argparse import ArgumentParser
 
@@ -7,6 +11,7 @@ def parse_args():
 
     return parser.parse_args()
 
+
 if __name__ == '__main__':
     from pm4py.read import read_pnml
     args = parse_args()
@@ -15,5 +20,15 @@ if __name__ == '__main__':
     pn, im, fm = read_pnml(petri_net_file, auto_guess_final_marking=True)
 
     r = PetriNetReification()
-    for fact in r.reify(pn, im, fm):
-        print(fact)
+    ctl = clingo.Control()
+    ctl.load('ilp_petri_net_repair/remap.lp')
+    with ctl.backend() as backend:
+        for fact in r.reify(pn, im, fm):
+            ctl_id = backend.add_atom(fact)
+            backend.add_rule([ctl_id])
+
+    ctl.ground([("base", [])])
+    with ctl.solve(yield_=True) as ctl_handler:
+        model = ctl_handler.model()
+        for symbol in model.symbols(shown=True):
+            print(symbol)
