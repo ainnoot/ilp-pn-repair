85b8885d8d5a9ccd648603b0b059b1bbe37d2d4b
ainnoot <ai6chr+github@gmail.com>
2024-05-10 16:43:01 +0200

    Script to discover Petri Nets using methods in pm4py

---
+++ discover_petri_net.py
@@ -0,0 +1,34 @@
+import pm4py
+import sys
+from pm4py.discovery import (
+	discover_petri_net_alpha,
+	discover_petri_net_inductive,
+	discover_petri_net_heuristics,
+	discover_petri_net_ilp
+)
+
+METHODS = {
+	'alpha': discover_petri_net_alpha,
+	'inductive': discover_petri_net_inductive,
+	'heuristics': discover_petri_net_heuristics,
+	'ilp': discover_petri_net_ilp
+}
+
+def parse_args():
+	if len(sys.argv) != 3:
+		print("Usage: {} [log] [method: {}]".format(__file__, "|".join(METHODS.keys())))
+		sys.exit(118)
+
+	log, method = sys.argv[1:] 
+	if method not in METHODS:
+		raise RuntimeError("Unknown method: got {} expected {}".format(method, METHODS))
+
+	log = pm4py.read_xes(log)
+	method = METHODS[method]
+	return log, method
+
+if __name__ == '__main__':
+	log, method = parse_args()
+	pn, im, fm = method(log)
+	pm4py.view_petri_net(pn, im, fm)
+	pm4py.save_vis_petri_net(pn, im, fm, "{}-{}.png".format(sys.argv[1], sys.argv[2]))
