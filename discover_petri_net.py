087c0a8111445a0909b99ef82975edd94ad054d4
ainnoot <ai6chr+github@gmail.com>
2024-05-10 21:48:50 +0200

    Code to discover models from XES using pm4py -- supports different algorithms, but we can work only with Alpha one

--- discover_petri_net.py
+++ discover_petri_net.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+
 import pm4py
 import sys
+
 from pm4py.discovery import (
 	discover_petri_net_alpha,
 	discover_petri_net_alpha_plus,
@@ -7,6 +10,9 @@ from pm4py.discovery import (
 	discover_petri_net_heuristics,
 	discover_petri_net_ilp
 )
+from pm4py.analysis import reduce_petri_net_invisibles
+from ilp_petri_net_repair import relabel_everything_because_i_dont_like_how_pm4py_names_things, \
+	check_equals_under_mapping
 
 METHODS = {
 	'alpha': discover_petri_net_alpha,
@@ -17,21 +23,32 @@ METHODS = {
 }
 
 def parse_args():
-	if len(sys.argv) != 3:
-		print("Usage: {} [log] [method: {}]".format(__file__, "|".join(METHODS.keys())))
+	if len(sys.argv) != 5:
+		print("Usage: {} [XES Log] [pm4py Discovery Method: {}] [PNML Output Directory] [Picture Directory]".format(__file__, "|".join(METHODS.keys())))
 		sys.exit(118)
 
-	log, method = sys.argv[1:] 
+	log, method, pnml_dir, fig_dir = sys.argv[1:]
 	if method not in METHODS:
 		raise RuntimeError("Unknown method: got {} expected {}".format(method, METHODS))
 
 	log = pm4py.read_xes(log)
 	method = METHODS[method]
-	return log, method
+	return log, method, Path(pnml_dir), Path(fig_dir)
+
 
 if __name__ == '__main__':
-	log, method = parse_args()
+	log, method, pnml_dir, fig_dir = parse_args()
 	pn, im, fm = method(log)
+	pn = reduce_petri_net_invisibles(pn)
+	(pn_r, im_r, fm_r), mapping = relabel_everything_because_i_dont_like_how_pm4py_names_things(pn, im, fm)
+	check_equals_under_mapping(pn, pn_r, mapping, im, im_r, fm, fm_r)
 
-	pm4py.save_vis_petri_net(pn, im, fm, "{}-{}.png".format(sys.argv[1], sys.argv[2]))
-	pm4py.write_pnml(pn, im, fm, "{}-{}.pnml".format(sys.argv[1], sys.argv[2]))
+	log_name = Path(sys.argv[1]).stem
+	fig_file_path = (fig_dir / log_name).with_suffix(".png")
+	pnml_path = (pnml_dir / log_name).with_suffix(".pnml")
+
+	print("Saving picture to", fig_file_path.as_posix())
+	pm4py.save_vis_petri_net(pn_r, im_r, fm_r, fig_file_path.as_posix())
+
+	print("Saving PNML to", pnml_path.as_posix())
+	pm4py.write_pnml(pn_r, im_r, fm_r, pnml_path.as_posix())
