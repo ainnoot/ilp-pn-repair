397b60782c8374122fbfeef2932b540029cd6318
altarzariol <alice.tarzariol@aau.at>
2024-07-02 16:51:36 +0200

    Change Experiments Set Up

--- create_damaged_models.sh
+++ create_damaged_models.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
-number_of_edits=(3 5 7 10 15 20)
-number_of_frozen_elements=(0 5 10 15)
+number_of_edits=(4 8 12 16)
+number_of_frozen_elements=(0 4 8 12)
+max_edit=16
+max_frozen=12
 PNML_DIR=Syntetic_data_new
 ILASP_DIR=experiments
 for PNML in $PNML_DIR/*.pnml; do
-    # https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
     FILENAME=$(basename -- "$PNML")
     LOG_NAME=${FILENAME%.*}
     NEW_DIR="$ILASP_DIR/$LOG_NAME"
     mkdir -p "$NEW_DIR"
+    # Create model
     python3 reify_petri_net.py $PNML $NEW_DIR/$LOG_NAME.model
-    python3 reify_log.py $PNML_DIR/$LOG_NAME.xes > $NEW_DIR/$LOG_NAME.examples
-    # Create damaged version (addition or remove)
+    # Create examples
+    python3 reify_log.py $PNML_DIR/$LOG_NAME.xes > $NEW_DIR/examples.las
+
+    # Create damaged version (add or remove $max_edit edges)
+    clingo damage_net.lp $NEW_DIR/$LOG_NAME.model --rand-freq=1 --sign-def=rnd --seed="$((RANDOM))" --const n="$max_edit" -V0  --out-atomf="%s." | ghead -n -1 | sed 's/\. /.\n/g' | shuf > $NEW_DIR/all_edits.txt
+
+    # Select $max_frozen nodes to freeze in the model (not involved in edits)
+    clingo frozen_net.lp $NEW_DIR/$LOG_NAME.model $NEW_DIR/all_edits.txt --rand-freq=1 --sign-def=rnd --seed="$((RANDOM))" --const n="$max_frozen" -V0  --out-atomf="%s." | ghead -n -1 | sed 's/\. /.\n/g' > $NEW_DIR/frozen_nodes.txt
+
+    
     for number in "${number_of_edits[@]}"
     do
-        clingo damage_net.lp $NEW_DIR/$LOG_NAME.model --rand-freq=1 --sign-def=rnd --seed="$((RANDOM))" --const n="$number" -V0  --out-atomf="%s." | ghead -n -1 | sed 's/\. /.\n/g' > $NEW_DIR/$LOG_NAME."$number"_random_edits.txt
-        for frozen in "${number_of_frozen_elements[@]}"
-        do
-            clingo frozen_net.lp $NEW_DIR/$LOG_NAME.model $NEW_DIR/$LOG_NAME."$number"_random_edits.txt --rand-freq=1 --sign-def=rnd --seed="$((RANDOM))" --const n="$frozen" -V0  --out-atomf="%s." | ghead -n -1 | sed 's/\. /.\n/g' > $NEW_DIR/$LOG_NAME."$number"_random_edits_"$frozen"_frozen_nodes.damaged_model
+        temp_file=$(mktemp)
+        # Pick $number edits
+        head -n $number $NEW_DIR/all_edits.txt  > $temp_file
 
-            clingo new_petri_net.lp $NEW_DIR/$LOG_NAME.model   $NEW_DIR/$LOG_NAME."$number"_random_edits.txt -V0  --out-atomf="%s." | ghead -n -1 | sed 's/\. /.\n/g' >> $NEW_DIR/$LOG_NAME."$number"_random_edits_"$frozen"_frozen_nodes.damaged_model
+        for frozen in "${number_of_frozen_elements[@]}"
+        do  
+            # Pick $frozen nodes 
+            head -n $frozen $NEW_DIR/frozen_nodes.txt  > $NEW_DIR/"$(printf "%02d" $number)"_random_edits_"$(printf "%02d" $frozen)"_frozen_nodes.damaged_model
+            # Create model with edits
+            clingo new_petri_net.lp $NEW_DIR/$LOG_NAME.model  $temp_file -V0  --out-atomf="%s." | ghead -n -1 | sed 's/\. /.\n/g' >> $NEW_DIR/"$(printf "%02d" $number)"_random_edits_"$(printf "%02d" $frozen)"_frozen_nodes.damaged_model
         done
     done
 done
--- create_lang_bias_and_solve.sh
+++ create_lang_bias_and_solve.sh
@@ -3,12 +3,14 @@ for ILASP_DIR in experiments/*/ ; do
     for MODEL in $ILASP_DIR*.damaged_model; do
         FILENAME=$(basename -- "$MODEL")
         LOG_NAME=${FILENAME%.*}
-        EX_NAME=${LOG_NAME%.*}
-        clingo language_bias.lp $ILASP_DIR$FILENAME -n 0 -V0 | sed -E 's/\(([^,]+),(.+)\)/\1 ~ \2./' | ghead -n -1 > $ILASP_DIR$LOG_NAME.search_space.lp
-        timeout 100 ILASP --version=2i background_knowledge.lp $ILASP_DIR$LOG_NAME.search_space.lp $ILASP_DIR$FILENAME $ILASP_DIR$EX_NAME.examples > $ILASP_DIR$LOG_NAME.ILP_edits.lp
+        # Create hypothesis space
+        clingo language_bias.lp $ILASP_DIR$FILENAME -W none -n 0 -V0 | sed -E 's/\(([^,]+),(.+)\)/\1 ~ \2./' | ghead -n -1 > $ILASP_DIR$LOG_NAME.search_space.lp
+        # Solve ILP task 
+        timeout 600 ILASP --version=2i background_knowledge.lp $ILASP_DIR$LOG_NAME.search_space.lp $ILASP_DIR$FILENAME $ILASP_DIR/examples.las > $ILASP_DIR$LOG_NAME.ILP_edits.lp
+        # If timeout, write comment
         exit_status=$?
         if [[ $exit_status -eq 124 ]]; then
-            echo "Timeout for: ${FILENAME}" > $ILASP_DIR$LOG_NAME.timeout.txt
+            echo "%Timeout for: ${FILENAME}" > $ILASP_DIR$LOG_NAME.ILP_edits.lp
         fi
     done
 done
