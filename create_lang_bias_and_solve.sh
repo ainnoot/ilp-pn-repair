22b473d4dba68b2214a0e78c27a1b0f6432c6b83
altarzariol <alice.tarzariol@aau.at>
2024-07-08 17:33:36 +0200

    Fix typo

--- create_lang_bias_and_solve.sh
+++ create_lang_bias_and_solve.sh
@@ -4,7 +4,7 @@ for ILASP_DIR in experiments/*/ ; do
         FILENAME=$(basename -- "$MODEL")
         LOG_NAME=${FILENAME%.*}
         # Create hypothesis space
-        clingo language_bias.lp $ILASP_DIR$FILENAME -W none -n 0 -V0 | sed -E 's/\(([^,]+),(.+)\)/\1 ~ \2./' | ghead -n -1 > $ILASP_DIR$LOG_NAME.search_space.lp
+        clingo language_bias.lp $ILASP_DIR$FILENAME -W none -V0 | sed -E 's/\(([^,]+),(.+)\)/\1 ~ \2./' | ghead -n -1 > $ILASP_DIR$LOG_NAME.search_space.lp
         # Solve ILP task 
         timeout 600 ILASP --version=2i background_knowledge.lp $ILASP_DIR$LOG_NAME.search_space.lp $ILASP_DIR$FILENAME $ILASP_DIR/examples.las > $ILASP_DIR$LOG_NAME.ILP_edits.lp
         # If timeout, write comment
